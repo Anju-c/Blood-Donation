616db35c731d1b7247a018ff36a6616537831cff
Betcy-Rajan <betcyrajan01@gmail.com>
2024-10-15 11:00:56 +0530

    DBMS Final

--- Save_Lives/lib/utilis/constants/api_constants.dart
+++ Save_Lives/lib/utilis/constants/api_constants.dart
@@ -2,14 +2,14 @@
 
 class APIConstants {
 
-  static const String tSecretAPIKey = "https://678f-103-170-55-163.ngrok-free.app/users/";
-  static const String donorsapi = "https://678f-103-170-55-163.ngrok-free.app/donors/";
-  static const String recipientsApi = "https://678f-103-170-55-163.ngrok-free.app/recipients/";
-  static const String userdonorsApi = "https://678f-103-170-55-163.ngrok-free.app/donors_with_user/";
-  static const String userRecipientApi = "https://678f-103-170-55-163.ngrok-free.app/recipients_with_user/";
-  static const String donorsSearchApi = "https://678f-103-170-55-163.ngrok-free.app/search_donors/?location=";
-  static const String donorsbloodApi = "https://678f-103-170-55-163.ngrok-free.app/search_bloodgroup/?bloodgroup=";
-  static const String donorsbloodlocApi = "https://678f-103-170-55-163.ngrok-free.app/search_donors_loc_bgp/";
-  static const String userApi =         "https://678f-103-170-55-163.ngrok-free.app/users/";
+  static const String tSecretAPIKey = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/users/";
+  static const String donorsapi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/donors/";
+  static const String recipientsApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/recipients/";
+  static const String userdonorsApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/donors_with_user/";
+  static const String userRecipientApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/recipients_with_user/";
+  static const String donorsSearchApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/search_donors/?location=";
+  static const String donorsbloodApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/search_bloodgroup/?bloodgroup=";
+  static const String donorsbloodlocApi = "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/search_donors_loc_bgp/";
+  static const String userApi =         "https://5ab2-2409-40f3-109f-6175-44ec-8081-76e4-805b.ngrok-free.app/users/";
 
 }
